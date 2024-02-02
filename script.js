3fb82836261e608252da93ab52ada390c324149a
shubham20242 <stekawad@mtu.edu>
2024-02-02 13:04:16 -0500

    Pacman classs created

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let canvas = document.getElementById("myCanvas");
 let c = canvas.getContext("2d");
 
-c.beginPath()
-//ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-c.arc(20,20,15,0,2*Math.PI)
-c.fillStyle = "yellow"
-c.fill()
-c.stroke() //its just giving a boundary
+class player
+{
+    constructor(x,y)
+    {
+        this.x = x;
+        this.y=y
+    }
 
+    draw()
+    {
+        c.beginPath()
+        //ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+        c.arc(this.x,this.y,15,0,2*Math.PI)
+        c.fillStyle = "yellow"
+        c.fill()
+        c.stroke() //its just giving a boundary
+    }
+}
+
+const pacman =  new player(20,20)
+pacman.draw()
