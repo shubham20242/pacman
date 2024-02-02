526479cf1e136f3c7a71717a2228bc624812063d
shubham20242 <stekawad@mtu.edu>
2024-02-02 18:51:40 -0500

    glitch pacman movement-2

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 //Global Variables
 let canvas = document.getElementById("myCanvas");
 let c = canvas.getContext("2d");
-let W_count = 0
-let A_count = 0
-let S_count = 0
-let D_count = 0
+let previousKey = null
+let currentKey = null
 
 class player
 {
-    
     constructor(x,y)
     {
         this.x = x
@@ -28,60 +25,29 @@ class player
         c.stroke() //its just giving a boundary
     }
 
-    move(key)
+    move(receiving_key)
     {
-        if(key ==`w`)
+        switch(receiving_key)
         {
-            if(W_count == 0)
-            {
-                doCount(key)
-                console.log(`w move`)
-                this.y = this.y - this.velocity_y
-                
-            }
-            
-        }
-        else if(key ==`s`)
-        {
-            if(S_count==0)
-            {
-                doCount(key)
-                console.log(`s move`)
-                this.y = this.y + this.velocity_y
-                
-            }
-          
-        }
-        else if(key ==`a`)
-        {
-            if(A_count ==0)
-            {
-                doCount(key)
-                console.log(`a move`)
+            case `w`:
+                this.y = this.y - this.velocity_y     
+                break
+            case `s`:
+                this.y = this.y + this.velocity_y  
+                break
+            case `a`:
                 this.x = this.x - this.velocity_x
-                
-            }
-            
+                break
+            case `d`:
+                //console.log("d invoked")
+                this.x = this.x + this.velocity_x 
+                break
         }
-        else if(key ==`d`)
-        {
-            if(D_count ==0)
-            {
-                doCount(key)
-                console.log(`d move`)
-                this.x = this.x + this.velocity_x
-                
-            }
-            
-        }
-        
     }
-
     clear()
     {
         c.clearRect(this.x-(this.radius),this.y-(this.radius),this.radius*2,this.radius*2)
     }
-
 }
 
 //Object Creation
@@ -89,60 +55,23 @@ const pacman =  new player(20,20)
 pacman.draw()
 
 //All functions
-function doCount(key)
-{
-    switch(key){
-        case `w`:
-            console.log(`w casw`)
-            W_count = 1
-            A_count = 0
-            S_count = 0
-            D_count = 0
-            break
-
-        case `s`:
-            console.log(`s casw`)
-            W_count = 0
-            A_count = 0
-            S_count = 1
-            D_count = 0
-            break
-
-        case `a`:
-            console.log(`a casw`)
-            W_count = 0
-            A_count = 1
-            S_count = 0
-            D_count = 0
-            break
-
-        case `d`:
-            console.log(`d casw`)
-            W_count = 0
-            A_count = 0
-            S_count = 0
-            D_count = 1
-            break
-            
-        
-    }
-
-}
-
-function gameLoop(key) {
+function gameLoop() {
+    console.log(`line 62: ${currentKey}`)
     pacman.clear()
-    pacman.move(key)
+    pacman.move(currentKey)
     pacman.draw()
 
-
-    requestAnimationFrame(() => gameLoop(key));
+    requestAnimationFrame(gameLoop);
 }
-
-
 
 //All Event Listners
 document.addEventListener(`keydown`,(event)=>{
-   
-    gameLoop(event.key)
+    currentKey = event.key
+    if(previousKey != currentKey)
+    {
+        gameLoop()
+        previousKey = currentKey
+    }
+    
 })
 
